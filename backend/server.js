9f5797bebd8d6e20fd81af4f08e836d833fa0c63
chandan kumar <chandankumar700451@gamil.com>
2025-11-23 12:57:52 +0530

    update server.js file for correct the cors on deploy

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 import authRoutes from "./routes/auth.js";
 import itemsRoutes from "./routes/items.js";
@@ -13,31 +11,29 @@ import orderRoutes from "./routes/order.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigin = process.env.CORS_ORIGIN;
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
+
+ 
 app.use(express.json());
 
-// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemsRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/order", orderRoutes);
 
- 
-
 const PORT = process.env.PORT || 4000;
-const allowedOrigin = process.env.CORS_ORIGIN;
-
-app.use(cors({
-    origin: allowedOrigin,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
-app.options("*", cors());
-
-
-app.use(express.json());
-
 
 mongoose
   .connect(process.env.MONGO_URI)
